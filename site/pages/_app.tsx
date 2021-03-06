44bbe7ae02830d56c0f5c8281b98106438d94412
Joe Yuhas <joseph.yuhas@gmail.com>
2021-03-06 17:16:54 -0500

    auth0 merge fixes

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,7 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Auth0Provider } from "@auth0/auth0-react";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain="blackburnian.us.auth0.com"
+      clientId="qBr5LcCLrn4VNv24LkzeF4jgc1t5NnYz"
+      redirectUri="http://localhost:3000"
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  )
 }
 
 export default MyApp
