67e2aec5428c33ae6f220f29a5246af62a7d757c
Dan Wilt <dwilt4rville@gmail.com>
2021-03-06 17:13:08 -0500

    converted to TS

---
+++ site/config.ts
@@ -0,0 +1,21 @@
+/**
+ * Global Config
+ */
+
+const config: {
+  domains: {
+    api: string;
+  };
+} = {
+  domains: {
+    /**
+     * API Domain
+     * Add the domain from your serverless express.js back-end here.
+     * This will enable your front-end to communicate with your back-end.
+     * (e.g. 'https://api.mydomain.com' or 'https://091jafsl10.execute-api.us-east-1.amazonaws.com')
+     */
+    api: 'https://5msjhlmwm3.execute-api.us-east-1.amazonaws.com',
+  },
+};
+
+export default config;
