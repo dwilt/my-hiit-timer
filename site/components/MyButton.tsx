46387fe068dcb9ea8e8504a2e9023d3ecc2d27fd
Dan Wilt <dwilt4rville@gmail.com>
2021-04-13 22:10:50 -0400

    Update MyButton.tsx

--- site/components/MyButton.tsx
+++ site/components/MyButton.tsx
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 import { blue } from '../variables/colors';
 
-export const MyButton = styled.button<{ isActive: boolean }>`
+export const MyButton = styled.button<{ isActive?: boolean }>`
   background: ${({ isActive }) => (isActive ? blue : 'orange')};
   border-radius: 3px;
   border: none;
